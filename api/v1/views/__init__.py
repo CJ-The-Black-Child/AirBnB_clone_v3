c0e7babdab47e067a67106730d7932b173ca1225
CJ-The-Black-Child <congomigue@gmail.com>
2023-10-01 16:38:33 +0000

    Updated api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,13 +2,13 @@
 """Creates The Blueprint instance"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-from api.v1.states import *
-from api.v1.cities import *
-from api.v1.amenities import *
-from api.v1.users import *
-from api.v1.places import *
-from api.v1.places_reviews import *
-from api.v1.views.places_amenities import *
+# from api.v1.states import *
+# from api.v1.cities import *
+# from api.v1.amenities import *
+# from api.v1.users import *
+# from api.v1.places import *
+# from api.v1.places_reviews import *
+# from api.v1.views.places_amenities import *
