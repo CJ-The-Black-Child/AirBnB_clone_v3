34292ef0dd9497aaa45ebcdb85f69c91c35afee6
Congo Justus Migue <congomigue@gmail.com>
2023-10-02 22:54:44 +0300

    Update to api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Creates The Blueprint instance"""
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
+from api.v1.states import *
+from api.v1.cities import *
+from api.v1.amenities import *
+from api.v1.users import *
+from api.v1.places import *
+from api.v1.places_reviews import *
+from api.v1.views.places_amenities import *
