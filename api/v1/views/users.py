aea77307ba3d0f861277721ee7daf6324bee8fa5
Congo Justus Migue <congomigue@gmail.com>
2023-10-02 13:22:41 +0300

    A better concise approach using app_views module

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,126 +8,109 @@ from flask import Flask, jsonify, abort, make_response, request
 from models import storage
 from models.user import User
 
-class UserView(app_views.View):
+@app_views.route('/users')
+def all_users():
     """
-    A view class for managing users.
+    Returns a list of all users in storage.
 
-    Attributes:
-        storage: A storage object for managing users.
+    Returns:
+        A list of users dictionaries.
     """
-    def __init__(self):
-        self.storage = storage
 
-    def all_users(self):
-        """
-        Returns a list of all users in storage.
+    users = []
+    for user in storage.all("User").values():
+        users.append(user.to_dict())
 
-        Returns:
-            A list of users dictionaries.
-        """
+        return jsonify(users)
 
-        users = []
-        for user in self.storage.all("User").values():
-            users.append(user.to_dict())
+@app_views.route('/users/<user_id>')
+def user(user_id):
+    """
+    Returns a user specified by id.
 
-            return jsonify(users)
+    Args:
+        user_id: The id of the user to retrieve.
 
-    def user(self, user_id):
-        """
-        Returns a user specified by id.
+    Returns:
+        A user dictionary.
+    """
 
-        Args:
-            user_id: The id of the user to retrieve.
+    user = storage.get("User", user_id)
+    if not user:
+        abort(404)
 
-        Returns:
-            A user dictionary.
-        """
+    return jsonify(user.to_dict())
 
-        user = self.storage.get("User", user_id)
-        if not user:
-            abort(404)
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """
+    Deletes a user specified by id.
 
-        return jsonify(user.to_dict())
+    Args:
+        user_id: The id of the user to deleteself.
 
-    def delete_user(self, user_id):
-        """
-        Deletes a user specified by id.
+    Returns:
+        A 200 Ok response.
+    """
 
-        Args:
-            user_id: The id of the user to deleteself.
+    user = storage.get("User", user_id)
+    if not user:
+        abort(404)
 
-        Returns:
-            A 200 Ok response.
-        """
+    storage.delete(user)
+    storage.save()
 
-        user = self.storage.get("User", user_id)
-        if not user:
-            abort(404)
+    return make_response(jsonify({}), 200)
 
-        self.storage.delete(user)
-        self.storage.save()
-
-        return make_response(jsonify({}), 200)
-
-    def add_user(self):
-        """
-        Creates a new user.
+@app_views.route('/users', methods=['POST'])
+def add_user():
+    """
+    Creates a new user.
         
-        Returns:
-            A 201 Created response with the new user dictionary in the body.
-        """
+    Returns:
+        A 201 Created response with the new user dictionary in the body.
+    """
 
-        if not request.get_json():
-            abort(400, description="Not a JSON")
+    if not request.get_json():
+        abort(400, description="Not a JSON")
 
-        if not request.get_json().get("email"):
-            abort(400, description="Missing email")
+    if not request.get_json().get("email"):
+        abort(400, description="Missing email")
 
-        if not request.get_json().get("password"):
-            abort(400, description="Missing password")
+    if not request.get_json().get("password"):
+        abort(400, description="Missing password")
 
-        user = User()
-        user.email = request.get_json()["email"]
-        user.password = request.get_json()["password"]
-        user.save()
+    user = User()
+    user.email = request.get_json()["email"]
+    user.password = request.get_json()["password"]
+    user.save()
 
-        return make_response(jsonify(user.to_dict()), 201)
+    return make_response(jsonify(user.to_dict()), 201)
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    """
+    Updates a user specified by id.
 
-    def update_user(self, user_id):
-        """
-        Updates a user specified by id.
+    Args:
+        user_id: The id of the user to update.
 
-        Args:
-            user_id: The id of the user to update.
+    Returns:
+        A 200 OK response with the updated user dictionary in the body.
+    """
 
-        Returns:
-            A 200 OK response with the updated user dictionary in the body.
-        """
+    user = storage.get("User", user_id)
+    if not user:
+        abort(404)
 
-        user = self.storage.get("User", user_id)
-        if not user:
-            abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
 
-        if not request.get_json():
-            abort(400, description="Not a JSON")
+    for key, value in request.get_json().items():
+        if key in ("id", "created_at", "updated_at", "email"):
+            continue
+        else:
+            setattr(user, key, value)
 
-        for key, value in request.get_json().items():
-            if key in ("id", "created_at", "updated_at", "email"):
-                continue
-            else:
-                setattr(user, key, value)
+    storage.save()
 
-        self.storage.save()
-
-        return make_response(jsonify(user.to_dict()), 200)
-
-user_view = UserView()
-
-app_views.add_url_rule("/users", view_func=user_view.all_users)
-app_views.add_url_rule("/users/<user_id>", view_func=user_view.user)
-app_views.add_url_rule("/users/<user_id>", methods=["DELETE"],
-        view_func=user_view.delete_user)
-app_views.add_url_rule("/users", methods=["POST"],
-        view_func=user_view.add_user)
-app_views.add_url_rule("/users/<user_id>", methods=["PUT"],
-        views_func=user_view.update_user)
+    return make_response(jsonify(user.to_dict()), 200)
